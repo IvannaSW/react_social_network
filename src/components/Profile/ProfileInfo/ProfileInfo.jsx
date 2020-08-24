74f98a92ad0fe6071bbe04148db5aa34710cbbee
IvannaSW <ivanna.sav08@gmail.com>
2020-08-24 14:06:35 +0300

    Start using hooks for Profile Status component

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,6 +3,7 @@ import s from './ProfileInfo.module.css';
 import profileReducer from '../../../redux/profile-reducer';
 import Preloader from '../../common/Preloader/Preloader';
 import ProfileStatus from './ProfileStatus';
+import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 
 const ProfileInfo = (props) => {
     if (!props.profile){
@@ -17,7 +18,7 @@ const ProfileInfo = (props) => {
                 <img src={props.profile.photos.large} />
                 <div>{props.profile.aboutMe}</div>
             </div>
-            <ProfileStatus status={props.status} updateStatus={props.updateStatus} />
+            <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} />
         </div>
     );
 }
